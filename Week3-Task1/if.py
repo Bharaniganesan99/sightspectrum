e244ba12c6b10bd1aa34281f94f6cfa1063716c7
unknown <Bharaniganesan99>
2023-06-08 10:02:10 +0530

    first commit

---
+++ Week3-Task1/if.py
@@ -0,0 +1,39 @@
+##print("----if statement----")
+##a = 33
+##b = 200
+##if b > a:
+##  print("b is greater than a")
+  
+
+##print("----identation error-----")
+##a = 33
+##b = 200
+##if b > a:
+##    print("b is greater than a") # you will get an error
+
+##
+##print("----elif----")
+##a = 33
+##b = 33
+##if b > a:
+##  print("b is greater than a")
+##elif a == b:
+##  print("a and b are equal")
+##
+##
+##print("----else----")
+##a = 200
+##b = 33
+##if b > a:
+##  print("b is greater than a")
+##elif a == b:
+##    
+##  print("a and b are equal")
+##else:
+##  print("a is greater than b")
+##
+##print("----short hand if----")
+##if a > b: print("a is greater than b")
+##
+
+print("gff")
---
+++ Week3-Task1/loop.py
@@ -0,0 +1,110 @@
+#while loop
+i=0
+while i<6:
+    print(i)
+    i+=1
+
+#break
+
+i=0
+while i<6:
+    print(i)
+    if i==3:
+        break
+    i+=1
+
+#continue
+print("CONTINUE")
+i=0
+while i<6:
+    #print(i)
+    i+=1
+    if i==3:
+        continue
+    print(i)
+
+print("else")
+i = 1
+while i < 6:
+  
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+##var="hi bharani"
+##print('"%s"' %var)
+##
+##
+##
+##print("----odd and even string with index----")
+##string=input()
+##for j,k in enumerate(string): 
+##    if j % 2 == 0: 
+##        print("{},{}".format(j,k))
+##        #pass
+##    else:
+##        print(" {}, {}".format(j,k))
+##
+##
+##s = 'bharani'
+##for k in range(len(s)):
+##  if (k%2==1):
+##    print(s[k],end="")
+##
+##var="old is gold"
+##if var == "o":
+##  print("success")
+##else:
+##  print("end")
+##
+##var="tit for tat"
+##for i in len(var):
+##  
+##  #print(len(var))
+##print(len(var))
+##
+##var=["a"]
+##for i in var:
+##  var.append(i.upper())
+##  print(var)
+##  
+##
+##var=["j","i","k","l"]
+##print(var[0])
+##print(var[2])
+##
+##my_list = ["a", "b", "c", "d", "e", "f", "g","h"]
+##print("odd")
+##for i in range(0,len(my_list),2):
+##  result=(list(my_list[i]))
+##  print(result,end="")
+##  print(type(result))
+##for i in range(1,len(my_list),2):
+##    result=(list(my_list[i]))
+##    print(result,end="")
+##
+##my_list = ["a", "b", "c", "d", "e"]
+##
+##output_list = []
+##for index, value in enumerate(my_list):
+##  if index == 0:
+##    
+##    output_list.append([index, value])
+##
+##print(output_list)
+##
+##var=["a","b","c","d"]
+##for i,j in enumerate(var[:]):
+####  print(i,j)
+##  if i%2==0:
+##    print(i)
+####    pass
+##  else:
+##    var.remove(j)
+##print(var)
+##          
+##
+##
+##
+##
