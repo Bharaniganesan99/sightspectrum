acd6e9087ab780dec5d404c5f2aa9b0b552c7d81
unknown <Bharaniganesan99>
2023-06-20 15:01:07 +0530

    first commit

---
+++ Week5-Task1/filesample.py
@@ -0,0 +1,89 @@
+##f=open("sample.txt","r")
+##f=open("sample.txt","w")
+##f.write("It opens the file to write and read both. It is different from r+ in the sense that it overwrites the previous file if one exists whereas r+ doesn't overwrite the previously written file. It creates a new file if no file exists. The file pointer exists at the beginning of the file.")
+####f.write("sample.txt","a")
+####f.write("age is 24")
+##
+##f.close()
+
+
+
+##f=open("sample.txt","r")
+##f=open("sample.txt","w")
+##f.write("my name is bharani")
+##var="age is 24"
+##f.write(var)
+##f.close()
+
+##f=open("sample.txt","r")
+##print(f.read(5))
+
+##f=open("sample.txt","r")
+##f=open("sample.txt","w")
+##f.write("my name is bharani")
+##f.close()
+####var="age is 24"
+####f.write(var)
+
+
+##for i in range(1,3):
+##    line="enter the string",format
+##    f.write(line)
+##f.close()
+
+##
+##with open("sample.txt","r+") as file:
+##    reading=file.read()
+##writing=reading+"fjyfkydy"
+##print(writing)
+
+
+##f=open("sample1.txt","r+")
+####f=open("sample1.txt","w")
+##f.write("age is 24")
+##f.close()
+##with open("sample.txt", "r") as file:
+##    data = file.readlines()
+##    for line in data:
+##        word = line.split()
+##        print(word)
+##
+
+
+
+##f=open("sample.txt","r")
+##f=open("sample.txt","w")
+##f.write("my name is bharani")
+##var="age is 24"
+##f.write(var)
+##f.write(var)
+##f.close()
+
+
+
+##with open("sample.txt","r+") as file:
+##    reading=file.read()
+##writing=reading+"fjyfkydy"
+##print(writing)
+
+##with open("sample.txt","a") as file:
+##    file.write("okay")
+####
+##with open("sample.txt","r") as file:
+##    f1=file.read()
+##    print(f1)
+##    f2=file.read()
+##    print(f2)
+
+##f=open("sample.txt","r")
+##f.read()
+##f.read()
+##f.close()
+##    
+
+
+##with open("sample.txt","w+") as file:
+##    var=input()
+##    file.write(var)
+##
+
